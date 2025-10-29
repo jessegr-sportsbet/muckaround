e9168ba4d8de44a3fe51cd3a34c20a30292923f1
Jesse Green <jesse.green@sportsbet.com.au>
2025-10-30 10:09:46 +1100

    da

--- test.sh
+++ test.sh
@@ -1,9 +1,11 @@
 #!/bin/zsh
 
-gh pr create -B main -d -t "Release: dummy" -b ""
+PR_URL=$(gh pr create -B main -d -t "Release: dummy" -b "")
+
 
 if [ $? -ne 0 ]; then
   echo "SNAFU"
 else
   echo "We good"
 fi
+echo "PR_URL is: $PR_URL"
